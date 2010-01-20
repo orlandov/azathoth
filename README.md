74c78837a97928aec0321c924bcf56729e65ec08
Orlando Vazquez <ovazquez@gmail.com>
2010-01-20 00:59:24 -0800

    Initial commit of the Azathoth http load tool

---
+++ README.md
@@ -0,0 +1,48 @@
+NAME
+----
+
+azathoth - HTTP benchmark and load generator tool
+
+SYNOPSIS
+--------
+
+    azathoth <options> http://example.com:8080/
+
+DESCRIPTION
+-----------
+
+azathoth is for generating lots of HTTP traffic. By utilizing Node.js's powerful
+asynchronous abilities it's possible to create an enormous number of requests.
+
+    Outside the ordered universe that amorphous blight of nethermost confusion
+    which blasphemes  and bubbles at the center of all infinity; the boundless
+    daemon sultan Azathoth, whose name no lips dare speak aloud, and who gnaws
+    hungrily in inconceivable, unlighted chambers beyond time and space amidst
+    the muffled, maddening beating of vile drums and the thin monotonous whine
+    of accursed flutes.                     The Dream Quest -- H. P. Lovecraft
+
+OPTIONS
+-------
+    
+    -c Number of concurrent connections to to use. Each client will make a
+       number of requests equal to this number divided by the number of total
+       requests.
+
+    -n Total number of requests to make.
+
+    -m HTTP method to use (default: GET)
+
+
+ENVIRONMENT
+-----------
+
+
+AUTHOR
+------
+
+Orlando Vazquez <ovazquez@gmail.com>
+
+SEE ALSO
+--------
+
+ab(1)
--- azathoth.js
+++ azathoth.js
@@ -1,38 +1,125 @@
 #!/usr/bin/env node
 
-var sys=require('sys'), http = require('http');
-var target = "jsconf";
+var sys = require('sys'),
+   http = require('http');
 
-var num_clients = 10;
-jjj = JSON.stringify;
-var num_requests = 100;
+jjj = JSON.stringify
+
+// Default options
+var method = "GET";
+var host = "localhost";
+var port = 80;
+var numClients = 10;
+var numRequests = 3000;
+var reqPerClient = numClients / numRequests;
+var path = "/";
+
+function stats(v) {
+    var l = v.length
+
+    var mean = v.reduce(function (a, b) { return a + b }) / l;
+
+    s = 0;
+
+    var s = 0;
+    v.forEach(function (val) {
+        s += Math.pow(val - mean, 2);
+    });
+    var variance = s / l;
+    var deviation = Math.sqrt(variance);
+
+    return { variance: variance, mean: mean, deviation: deviation };
+}
+
+var elapsedStart;
+var elapsedTime;
+var totalTime = 0;
+var bytesTransferred = 0;
+var times = [];
+var clientId = 0;
+
+function pad(str, width) {
+    return (new Array(width-str.length)).join(" ") + str;
+}
+
+function printReportItem(name, val) {
+    sys.puts(pad(name + ":", 40) + " " + val);
+}
+
+function printReport(report) {
+    //sys.puts("Server Software:"  Cherokee/0.99.38
+    sys.puts("");
+    printReportItem("Server Hostname", host);
+    printReportItem("Server Port", port)
+
+    printReportItem("HTTP Method", method)
+    printReportItem("Document Path", path)
+    //printReportItem("Document Length"  2141 bytes
+
+    printReportItem("Concurrency Level", numClients);
+    printReportItem("Number of requests", numRequests);
+    //printReportItem("Time taken for tests"  0.120 seconds)
+    //printReportItem("Complete requests", 1)
+    //printReportItem("Failed requests"  0)
+    //printReportItem("Write errors"  0)
+    //rintReportItem("Total transferred"  2344 bytes)
+    //printReportItem("HTML transferred"  2141 bytes)
+    printReportItem("Body bytes transferred", bytesTransferred);
+    printReportItem("Elapsed time", elapsedTime*1000);
+    printReportItem("Time spent waiting on requests", totalTime*1000);
+    printReportItem("Requests per second", (1000*report.stats.mean/elapsedTime));
+    printReportItem("Mean time per request", report.stats.mean);
+    printReportItem("Time per request standard deviation", report.stats.deviation);
+    //printReportItem("Time per request", report.mean + "[ms] (mean)");
+    //printReportItem("Transfer rate"  19.08 [Kbytes/sec] received)
+}
+
+
+function doClientRequests() {
+    var j = 0;
+    //sys.puts("Client " +clientId+  " reporting in!");
+    var myId = clientId;
+        var connection = http.createClient(port, host);
+    function doRequest() {
+        if (++j > numRequests/numClients) return;
+
+        var start = (new Date()).getTime();
+        var request = connection.request(method, "/", { "host": host });
+        request.finish(function(response) {
+            var end = (new Date()).getTime();
+            var	delta = end - start;
+            times.push(delta);
+            totalTime += delta;
+	    //sys.puts("Client " +myId+  " received response in " +delta+ "ms.");
+            var len = times.length;
+
+            if ((len % (numRequests/10)) == 0) {
+                sys.puts(pad("Completed " +times.length+ " requests", 40) + " ("+ (len/numRequests*100) + "%)");
+            }
+            if (len == numRequests) {
+                elapsedTime = (new Date()) - elapsedStart;
+                var s = stats(times);
+                var report = {
+                    stats: s
+                }
+                printReport(report);
+
+                return;
+            }
+            response.addListener("body", function (body) {
+                bytesTransferred += body.length;
+            });
+        });
+        setTimeout(arguments.callee, 0);
+    }
+    setTimeout(doRequest, 0);
+}
 
 function main() {
-    var times = [];
-
-    for (var i = 0; i < num_clients; i++) {
-        var connection = http.createClient(8000, "localhost");
-        for (var j = 0; j < num_requests/num_clients; j++) {
-            (function () {
-                var request = connection.request("GET", "/", {"host": "localhost"});
-                sys.puts("loopity");
-                var start = (new Date()).getTime();
-                request.finish(function(response) {
-                    var mstart = start;
-                    var end = (new Date()).getTime();
-
-                    sys.puts("start = " + mstart);
-                    sys.puts("end = " + end);
-                    times.push(end-mstart);
-                    response.addListener("complete", function() {
-                        if ((times.length % (num_requests/num_clients)) == 0) {
-                            //sys.puts("average is "+(times.reduce(function (a, b) { return a+b }, 0)/times.length));
-                            sys.puts("average is "+jjj(times));
-                        }
-                    });
-                });
-            })();
-        }
+    elapsedStart = new Date();
+    for (var i = 0; i < numClients; i++) {
+        doClientRequests();
+        clientId++;
     }
 }
 
